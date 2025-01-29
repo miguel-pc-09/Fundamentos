52c40a7f6d59e88502266af1d1eca2154e6bcefc
miguel-pc-09 <m.peruchacastello@gmail.com>
2025-01-29 17:40:32 +0100

    update
    Seguir con la introdución de distancia y el tiempo y los guarde. Aparte que lo sume a lo que ya tuviera.

--- Tarea2ºTrimestre/GestionesUsuarios/Program.cs
+++ Tarea2ºTrimestre/GestionesUsuarios/Program.cs
@@ -1,4 +1,4 @@
-﻿// Programa permita gestionar sesiones para poder introducir sus marcas de entremainetos 
+﻿// Programa permita gestionar sesiones para poder introducir sus marcas de entrenamietos 
    // Sistema permitira:
        // Registrar usuarios: correo y contraseña. no podran repetir usuarios mismo nombre
        // Logear usuario: pidiendo correo y una contraseña. Si exite, el sistema dejara las siguientes acciones
@@ -7,8 +7,34 @@
            //vacias entrenamientos: vaciara la lsita de todos los entrenamientos
            // cerrrar sesion
 public class Programa{
+    Scanner teclado = new Scanner(System.in);
 
     public static void Main(string[] args){
-         
+        
+        //Bienvenida a la app
+         Console.WriteLine("Bienvenido a tu gestor de entrenamiento");
+        
+         //Menu de registro 
+         Console.WriteLine("Comencemos con tu registro:");
+         //Corre electronico 
+         Console.WriteLine("Introduce tu correo electronico: ");
+         string correo = Console.ReadLine();
+
+         //Metodo para validar el correo 
+         if(correo == "correo"){
+            Console.WriteLine("El correo ya fue registrado");
+         }else{
+            Console.WriteLine("Correo registrado");
+         }
+
+         //Pide contraseña 
+         Console.WriteLine("Introduce tu contraseña: ");
+         int contraseña = int.Parse(Console.ReadLine());
+          
+          // Registrar un entrenamiento: Pedir distancia recorrida y tiempo
+          Console.WriteLine("Introduce la distancia recorrida: ");
+
+        
+
  }
 }
