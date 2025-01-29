68364be00171f5fe4ab7e46f357bdf5a5e046857
miguel-pc-09 <m.peruchacastello@gmail.com>
2025-01-29 16:04:09 +0100

    update
    Comienzo de la tarea

--- Tarea2ºTrimestre/GestionesUsuarios/Program.cs
+++ Tarea2ºTrimestre/GestionesUsuarios/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Programa permita gestionar sesiones para poder introducir sus marcas de entremainetos 
+   // Sistema permitira:
+       // Registrar usuarios: correo y contraseña. no podran repetir usuarios mismo nombre
+       // Logear usuario: pidiendo correo y una contraseña. Si exite, el sistema dejara las siguientes acciones
+           // Registrar un entrenamiento: donde pedira la distancia recorrida y el tiempo empleado
+           // listar entrenamientos: donde se mostraran todos los entrenamientos introducidos
+           //vacias entrenamientos: vaciara la lsita de todos los entrenamientos
+           // cerrrar sesion
+public class Programa{
+
+    public static void Main(string[] args){
+         
+ }
+}
