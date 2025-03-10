22572b055468931d8aac97aac6827c674247cd09
miguel-pc-09 <m.peruchacastello@gmail.com>
2025-03-10 18:29:14 +0100

    update

---
+++ Tema 1/Tema 1/Program.cs
@@ -0,0 +1,83 @@
+﻿// Ejercicios tema 1
+
+using System.ComponentModel;
+using System.Security.Cryptography.X509Certificates;
+
+class Entrada 
+{
+    public static void Main (string[]args)
+    {
+        
+        // Ejercicio 1 declarar constantes locales. nombre, direccion calle codigo postal pai
+        //ejercicioUno();
+
+        //Ejercicio 2Programa que lee el nombre completo y la edad de una persona, y muestra los datos leídos.
+        //ejercicioDos();
+
+        //Ejercicio 3 Hágase un programa que lea dos variables enteras y obtenga las siguientes operaciones:
+        //ejercicioTres();
+
+        //Ejercicio 4 Unos amigos entra en un bar que ofrece las bebidas a 1,25€ y los bocadillos a 2,05€. El camarero les pregunta cuántas bebidas y bocadillos quieren. Calcula el coste de la consumición, mostrando primero el coste de las bebidas y de los bocadillos. (Bar)
+        ejercicioCuatro();
+       
+        }
+        // Ejercicio 1 declarar constantes locales. nombre, direccion calle codigo postal pai
+        /* static  void ejercicioUno()
+        {
+
+         Console.WriteLine("Nacho Garcia");
+         Console.WriteLine("c/buenos aires 2");
+         Console.WriteLine("28567 Madrid");
+         Console.WriteLine ("Espana");
+    
+        } */
+
+        //Ejercicio 2Programa que lee el nombre completo y la edad de una persona, y muestra los datos leídos.
+       /*  static void ejercicioDos()
+        {
+            Console.WriteLine("Escribe tu nombre comppleto: ");
+            string nombre =  (Console.ReadLine());
+            Console.WriteLine("Escribe tu edad :");
+            int edad = int.Parse(Console.ReadLine());
+            Console.WriteLine("Te llamas "+nombre);
+            Console.WriteLine("Tu edad es: "+edad);
+            Console.WriteLine("Pulsa enter para continuar...");
+        } */
+
+        // Ejercicio 3 Hágase un programa que lea dos variables enteras y obtenga las siguientes operaciones:
+  /*       static void ejercicioTres()
+        {
+            Console.WriteLine("Introduzca el primer operando: ");
+            int oper1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Introduce el 2 operando");
+            int oper2 = int.Parse(Console.ReadLine());
+            int suma = oper1 + oper2;
+            Console.WriteLine("La suma de los 2 numeros es: "+suma);
+
+        } */
+
+        //Ejercicio 4 Unos amigos entra en un bar que ofrece las bebidas a 1,25€ y los bocadillos a 2,05€. El camarero les pregunta cuántas bebidas y bocadillos quieren. Calcula el coste de la consumición, mostrando primero el coste de las bebidas y de los bocadillos. (Bar)
+        /* static void ejercicioCuatro()
+        {
+            double bebida = 0;
+            double bocadillo = 0;
+           Console.WriteLine("Numero de bebidas: ");
+           int bebidas = int.Parse(Console.ReadLine());
+           Console.WriteLine("Numero de bocadillos: ");
+           int bocadillos = int.Parse(Console.ReadLine());
+           Console.WriteLine("Coste de las bebidas: 1.75");
+           bebida = bebidas * 1.75;
+           Console.WriteLine("Coste de los bocadillos: 2.25");
+           bocadillo = bocadillos * 2.25;
+           double consumicion = bebida + bocadillo;
+           Console.WriteLine("Coste de la consumicion: "+consumicion);
+        } */
+        
+        // ejercicio 5 
+        static void ejercicioCinco()
+        {
+            
+        }
+
+
+}
