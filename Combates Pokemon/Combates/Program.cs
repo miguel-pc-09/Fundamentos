b3d2d7621ea1da6c9bd293e0f1b6a42831192389
miguel-pc-09 <m.peruchacastello@gmail.com>
2025-03-15 10:59:26 +0100

    update

---
+++ Combates Pokemon/Combates/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Security.Cryptography;
+
+public class MyPlay
+{
+    public static void Main(string[] args)
+    {
+        //Bienvenida al juego
+        Console.WriteLine("Bienvenido a combates pokemon ");
+
+        // Pide nombre al jugador
+        Console.WriteLine("¿Cuál es tu nombre?: ");
+        string? nombre = Console.ReadLine();
+        Console.WriteLine("Bien "+nombre+", ¿Qué pokemon quieres para combatir?: ");
+
+    }
+}
