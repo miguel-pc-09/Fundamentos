dff2ffb3ff6ff194f0c3c99271511c0d32d99f80
miguel-pc-09 <m.peruchacastello@gmail.com>
2025-03-23 14:59:44 +0100

    update

--- Ejercicio1Presencial/concesionario/obj/Debug/net9.0/concesionario.AssemblyInfo.cs
+++ Ejercicio1Presencial/concesionario/obj/Debug/net9.0/concesionario.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("concesionario")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbcee190c2cd5d9ac4f913bc9328586921d4b6d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1cd17ab41d0be1f3cee49a7c0a2006f157439cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("concesionario")]
 [assembly: System.Reflection.AssemblyTitleAttribute("concesionario")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
