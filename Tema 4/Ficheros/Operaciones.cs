b11602f493f3a2b383e678fcb7f99e53dc69d948
miguel-pc-09 <m.peruchacastello@gmail.com>
2025-04-15 13:54:13 +0200

    update

--- Tema 4/Ficheros/Operaciones.cs
+++ Tema 4/Ficheros/Operaciones.cs
@@ -9,14 +9,29 @@ class Operaciones
 
         //3º Creacion del file para ver diferencia con el de Java
         //File.GetLastAccessTime(path); // File es una clase estatica, conjunto de metodos estatico y como tal podemos poner File.(elegir)
+
         // Lo sacaremos por consola con Console.WriteLine
-        Console.WriteLine("Last Access Time: "+ File.GetLastAccessTime(path)); // Al ejecutar nos dira Last... y la fecha y hora en el momento de la ejecucion
+     //    Console.WriteLine("Last Access Time: "+ File.GetLastAccessTime(path)); // Al ejecutar nos dira Last... y la fecha y hora en el momento de la ejecucion
         
         // 5º Atributos de la clase file
-        Console.WriteLine("Atributos: "+ File.GetAttributes(path));// Devolvera un normal 
+     //  Console.WriteLine("Atributos: "+ File.GetAttributes(path));// Devolvera un normal 
 
         // 6º Preguntar si existe
-        Console.WriteLine("Exists: "+ File.Exists(path));
+     // Console.WriteLine("Existe: "+ File.Exists(path)); // Devolvera un True o False 
+
+        // Los metodos Los metodos Exits, GetLastAccessTime, GetAttributes, pertenece a la case. Un elemento que es estatico no necesita del objeto de tipo file
+        // para ejecutar el metodo correspondiente.  
+
+        // 7º Ahora decimos que queremos guardar o acceder a un objeto de tipo de file. Anteriormente accedi de forma directa, pero a veces lo queremos guardar en variables
+        // , lo queremos persistir, o llevar a una variable de entorno, lo que se nos ocurra.
+        FileInfo fileInfo = new FileInfo(path);
+        Console.WriteLine("Last Access Time: "+ fileInfo.LastAccessTime); // Esto ya es un metodo normal y corriente. Tendremos lo mismo pero en vez de pertenecer a la clase,
+        Console.WriteLine("Atributo: "+ fileInfo.Attributes); // pertenece al objeto.
+        Console.WriteLine("Tamaño: "+ fileInfo.Length); 
+        Console.WriteLine("Nombre del fichero: "+ fileInfo.Name); 
+        Console.WriteLine("Directorio: "+ fileInfo.DirectoryName); 
+        Console.WriteLine("Exist: "+ fileInfo.Exists);
+
     }
 
 }
--- Tema 4/Ficheros/Program.cs
+++ Tema 4/Ficheros/Program.cs
@@ -4,8 +4,8 @@
         Console.WriteLine("Trabajo con ficheros");
         // llamamos a operaciones para llamar a obtener informacion 
         Operaciones operaciones = new Operaciones();
-        // Dentro () meteremos el path del fichero que antes cogimos 
-        operaciones.Obtenerinfirmacion("Users/miguel/Documents/GitHub/Fundamentos/Tema 4/Ficheros/informacion.txt");
+        // Dentro () meteremos el path del fichero que antes cogimos Al ser Windows utilizar un @ y despues "path"
+        operaciones.Obtenerinfirmacion(@"C:\Users\migue\Documents\GitHub\Fundamentos\Tema 4\Ficheros\informacion.txt");
         //2º Vamos a Operaciones a crear el obtener informacion con el console
         // 4º Ejecutamos el Last...
     }
