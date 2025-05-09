3082d65767e3ec153807fcd83bc4f71f9eeac8f8
miguel-pc-09 <m.peruchacastello@gmail.com>
2025-05-09 19:43:36 +0200

    update

---
+++ Simulacro Mayo/Ejercicio3/NumerosEjericio3/Program.cs
@@ -0,0 +1,58 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        List<int> numeros = new List<int>();
+        Random aleatorio = new Random();
+
+        // Genera 20 numeros entre 1 y 100
+        for (int i = 0; i < 20; i++)
+        {
+            int numero = aleatorio.Next(1, 101);
+            numeros.Add(numero);
+        }
+
+        //mostrar todos los numeros 
+        Console.WriteLine("numeros generados: ");
+        foreach (int n in numeros)
+        {
+           Console.WriteLine(n + " "); 
+        }
+        Console.WriteLine();
+
+        // Calcular datos
+        int pares = 0;
+        int impares = 0;
+        int suma = 0;
+        int mayor = numeros[0];
+        int menor = numeros[0];
+
+        foreach (int n in numeros)
+        {
+            if(n % 2 == 0)
+            {
+                pares++;
+            }
+            else
+            {
+                impares++;
+            }
+            if (n > mayor)
+            {
+                mayor = n;
+            }
+            if(n < menor)
+            {
+                menor = n;
+            }
+
+            suma += n;
+        }
+        // mostrar resultados
+        Console.WriteLine("Cantidad de pares: "+ pares);
+        Console.WriteLine("Cantidad de impares: " + impares);
+        Console.WriteLine("Número más grande: " + mayor);
+        Console.WriteLine("Número más pequeño: " + menor);
+        Console.WriteLine("Suma total: " + suma);
+    }
+}
