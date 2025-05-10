6c73af230223fece559bc805a6cf3fc2fe10f9cc
miguel-pc-09 <m.peruchacastello@gmail.com>
2025-05-10 17:24:21 +0200

    update

--- Tema 2/Agenda/Agenda/Program.cs
+++ Tema 2/Agenda/Agenda/Program.cs
@@ -13,6 +13,28 @@
         agenda.AgregarPersona("001E", "Beatriz");
         // Saldra un error, porque la clave esta repetida
 
-        agenda.ObtenerPersona("001A");
+        agenda.ObtenerPersona("001A"); // Devolvera la persona con este dni
+
+        // Pero si buscamos un dni que no esta...
+        agenda.ObtenerPersona("001Z"); // como le pusimos que diese el valor no imprimira nada pero no dara error
+
+        // mostrar el primero
+        agenda.MostrarPrimerDato();
+
+        // Si en el metodo pusieramos +value.Lenght -> error NullReferenceException 
+
+        agenda.BorrarDatos("001F"); // con la primera opcion -> no dara error si no un false 
+
+        //agenda.BorrarDatos("001A");// El primero deberia quedar borrado, Ahora cambiemos el void por bool 
+        if(agenda.BorrarDatos("001A"))
+        {
+            Console.WriteLine("Borrado correctamente. ");
+        }
+        else
+        {
+            Console.WriteLine("Fallo en el borrado");
+        }
+
+        agenda.ListarTodos();
     }
 }
